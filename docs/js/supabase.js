6caad7f7790ff7d63380cf1b7bd3e2ce8806f7a0
glimmerjewels <glimmerjewels2000@gmail.com>
2026-06-09 22:03:53 +0530

    Update supabase.js
    supabase auth

--- docs/js/supabase.js
+++ docs/js/supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_ANON_KEY";
+const SUPABASE_URL = "https://bwgcnxaedxfoaoivfqlz.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3Z2NueGFlZHhmb2FvaXZmcWx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MzY5ODksImV4cCI6MjA5MzUxMjk4OX0._ZfZdVWd1a_XqYKh1nWtdosc9PZQW85CSWkEpQmos38";
 
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
